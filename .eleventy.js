44ddd61031a8233e8d73539d0d10984f45edc14f
Jens Andreasson <jensandreasson77@gmail.com>
2022-04-06 09:41:44 +0200

    ✨ part 4

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,14 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget("./src/sass/");
+    eleventyConfig.addWatchTarget('./src/sass/');
 
-  return {
-    dir: {
-      input: "src",
-      output: "public",
-    },
-  };
+    if (process.env.ELEVENTY_ENV === 'prod') {
+        eleventyConfig.ignores.add('./src/__docs/');
+    }
+
+    return {
+        dir: {
+            input: 'src',
+            output: 'public',
+        },
+    };
 };

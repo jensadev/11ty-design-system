810100122d61409b454fc4ffe4ad4fc2f3366741
Jens Andreasson <jensandreasson77@gmail.com>
2022-04-26 20:25:59 +0200

    ✨ part 7

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,34 @@ module.exports = function (eleventyConfig) {
         eleventyConfig.ignores.add('./src/__docs/');
     }
 
+    eleventyConfig.addShortcode(
+        'card',
+        function ({
+            cardImage,
+            cardAlt,
+            cardTitle,
+            cardType,
+            featureLink,
+            featureLinkContent,
+        }) {
+            let cardClasses =
+                cardType === 'feature' ? 'card--feature-image' : '';
+            let cardContent = '';
+            if (cardType === 'feature') {
+                cardContent = `<a href="${featureLink}">${featureLinkContent}</a>`;
+            }
+            return `
+                <div class="card ${cardClasses}">
+                    <img src="${cardImage}" alt="${cardAlt}">
+                    <div class="card__content">
+                        <h3 class="card__title">${cardTitle}</h3>
+                        ${cardContent}
+                    </div>
+                </div>
+            `;
+        }
+    );
+
     return {
         dir: {
             input: 'src',
